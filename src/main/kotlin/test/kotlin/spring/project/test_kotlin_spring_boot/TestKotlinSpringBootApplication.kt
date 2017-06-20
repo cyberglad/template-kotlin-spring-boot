6e7950ab73a67ab66ba4e59986add20548bb0aa4
cyberglad <cyberglad@mail.ru>
2017-06-20 15:17:06 +0200

    Initial commit

---
+++ src/main/kotlin/test/kotlin/spring/project/test_kotlin_spring_boot/TestKotlinSpringBootApplication.kt
@@ -0,0 +1,41 @@
+package test.kotlin.spring.project.test_kotlin_spring_boot
+
+import com.fasterxml.jackson.annotation.JsonInclude
+import com.fasterxml.jackson.databind.DeserializationFeature
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.module.kotlin.registerKotlinModule
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration
+import org.springframework.boot.builder.SpringApplicationBuilder
+//import org.springframework.boot.context.web.SpringBootServletInitializer
+import org.springframework.boot.web.support.SpringBootServletInitializer
+import org.springframework.context.annotation.Bean
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter
+
+@SpringBootApplication
+@EnableAutoConfiguration(exclude = arrayOf(DataSourceAutoConfiguration::class))
+open class TestKotlinSpringBootApplication : SpringBootServletInitializer() {
+
+    @Bean
+    open fun mapperForKotlinTypes(): MappingJackson2HttpMessageConverter {
+        return MappingJackson2HttpMessageConverter().apply { objectMapper = jacksonMapper }
+    }
+
+    override fun configure(application: SpringApplicationBuilder): SpringApplicationBuilder =
+            application.sources(TestKotlinSpringBootApplication::class.java)
+
+    companion object {
+
+        val jacksonMapper = ObjectMapper().registerKotlinModule()
+                .setSerializationInclusion(JsonInclude.Include.NON_ABSENT)
+                .enable(DeserializationFeature.FAIL_ON_NULL_FOR_PRIMITIVES)
+
+        @Throws(Exception::class)
+        @JvmStatic fun main(args: Array<String>) {
+            println("starting application...")
+            SpringApplication.run(TestKotlinSpringBootApplication::class.java, *args)
+        }
+    }
+}
---
+++ src/test/kotlin/test/kotlin/spring/project/test_kotlin_spring_boot/TestKotlinSpringBootApplicationTests.kt
@@ -0,0 +1,16 @@
+package test.kotlin.spring.project.test_kotlin_spring_boot
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class TestKotlinSpringBootApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
